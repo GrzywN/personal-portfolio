891f9448b9da94c59bdc390021a1259cf940baac
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-09-12 20:22:22 +0200

    change / fix navbar layout for chrome based browsers

--- components/Navbar/Nav.tsx
+++ components/Navbar/Nav.tsx
@@ -16,8 +16,9 @@ function Nav({ onClick, isOpen, content }: NavProps) {
     <nav
       className={`
       ${isOpen ? 'scale-x-100 opacity-100' : 'scale-x-0 opacity-0'} 
-      fixed inset-x-8 bottom-4 top-24 grid place-items-center rounded-md bg-light-grey/90 duration-400 ease-in-out
-      lg:static lg:ml-8 lg:scale-x-100 lg:bg-transparent lg:opacity-100 lg:transition-none`}
+      absolute left-1/2 -bottom-8 grid h-[calc(100vh-9rem)] w-full max-w-[calc(100vw-4rem)]
+        -translate-x-1/2 translate-y-full place-items-center rounded-md bg-light-grey/90 duration-400 ease-in-out
+      lg:static lg:ml-8 lg:h-auto lg:w-auto lg:scale-x-100 lg:transform-none lg:bg-transparent lg:opacity-100 lg:transition-none`}
       id="primary-navigation"
     >
       <ul
