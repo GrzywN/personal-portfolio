1de9613cbdb09ae2a9c6ea3f95d8909c9cef6f46
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-10-04 20:14:57 +0200

    fix wrong url address in sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://karol.binkowski.me',
+  siteUrl: process.env.SITE_URL || 'https://karolbinkowski.me',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
